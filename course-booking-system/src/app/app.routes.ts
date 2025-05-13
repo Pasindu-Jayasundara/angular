819929d1da22757da80541969b8f77332717909d
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-05-13 15:14:49 +0530

    sign up form

--- course-booking-system/src/app/app.routes.ts
+++ course-booking-system/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { CourseListComponent } from './course-list/course-list.component';
 import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { AboutComponent } from './about/about.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'courses',pathMatch:'full'},
     {path:'courses',component:CourseListComponent},
     {path:'courses/:id',component:CourseDetailComponent},
+    {path:'about',component:AboutComponent},
+    {path:'sign-up',component:SignUpFormComponent},
 ];
